a4630ae5c8b3e0f958a1be8bbd75edaa86b79f4d
ia rigby <marvinzem@gmail.com>
2022-03-09 23:00:20 +0400

    9-IMPROVEMENT remove getlist command since students.json is now created automatically

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -2,4 +2,3 @@
 mkdir -p ../data
 # requires package.json workspaces to be listed in dependency order
 node ./scripts/getList.js | xargs -I {} sh -c "cd {} && yarn build"
-sh -c "cd classroom-api && yarn getlist -c 21f შესავალი ციფრულ ტექნოლოგიებში"
