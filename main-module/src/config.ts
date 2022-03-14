0118e7e32640cb398b9d4d5301893c4353c0fedf
lukaaduashvili <ladua18@freeuni.edu.ge>
2022-03-14 23:24:25 +0400

    14-IMPROVEMENT Move subject name from config.ts to data/subject.json

--- main-module/src/config.ts
+++ main-module/src/config.ts
@@ -1,11 +1,12 @@
 import path from 'path'
 import { data_path} from "./runs";
+import fs from "fs";
 
 // ამ ფაილში უნდა იყოს მხოლოდ და მხოლოდ default პარამეტრები 
 // იმ ყველაფრის, რაც user-ს შეუძლია რომ გადმოაწოდოს
 
 export const config = {
-    subject: '21f შესავალი ციფრულ ტექნოლოგიებში',
+    subject: JSON.parse(fs.readFileSync(path.resolve(__dirname, `${data_path}/subject.json`)).toString()).subject,
     STUDENTS_DATA_PATH: path.resolve(__dirname, `${data_path}/students.json`),
     CLASSROOM_CREDENTIALS_PATH: path.resolve(__dirname, `${data_path}/credentials/credentials.json`),
     CLASSROOM_TOKEN_PATH: path.resolve(__dirname, `${data_path}/credentials/token.json`)
