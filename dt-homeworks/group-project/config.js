2abbe4f1aa0be1dfb68538b11b9ad4c34936f1f8
ia rigby <marvinzem@gmail.com>
2022-07-09 19:15:29 +0400

    fix small bug for group project

--- dt-homeworks/group-project/config.js
+++ dt-homeworks/group-project/config.js
@@ -1,7 +1,9 @@
 const fs = require('fs')
 const path = require('path')
+
+const existingProjects =  []
 try {
-    const existingProjects = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../../data/projects.json'), 'utf-8'))
+    existingProjects.push(...JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../../data/projects.json'), 'utf-8')))
 } catch (e) {
     const existingProjects =  []
 }
