ed1dc746dcbf67ce278e07dc03553b9973327972
ia rigby <marvinzem@gmail.com>
2022-11-01 16:08:49 +0400

    36 bugfix main module tests (#37)
    * 36-BUGFIX fix name for homeworkChecker test, ignore case that is not working
    
    * 36-BUGFIX move non integration tests to their own file
    
    * 36-BUGFIX remove console log when testing notifications
    
    * 36-BUGFIX fix the integration test showing up as passing during the error
    
    * 36-BUGFIX paths refactor
    - move default paths from various files to config. This also fixes the circular dependency causing a variable to be undefined
    - replace string concatenation with path join
    
    * 36-BUGFIX only add project scores to final results if they exist
    
    * 36-BUGFIX fix erroneous platform dependent path reading
    
    * 36-BUGFIX update webHwInterface namae
    
    * 36-BUGFIX ignore tests that require compiled server tests
    
    * 36-BUGFIX webstorm test command

--- dt-homeworks/hw6/config.js
+++ dt-homeworks/hw6/config.js
@@ -2,7 +2,7 @@ module.exports = {
     id: 'hw6',
     classroomName: 'დავალება 6',
     deadline: '2021-12-02',
-    testFileName: '../../../webHwInterface/server-tests/hw6/server.js',
+    testFileName: '../../../jspilot/server-tests/hw6/server.js',
     module: 'web',
     emailTemplates: {}
 }   
--- dt-homeworks/hw7/config.js
+++ dt-homeworks/hw7/config.js
@@ -2,7 +2,7 @@ module.exports = {
     id: 'hw7',
     classroomName: 'დავალება 7',
     deadline: '2021-12-23',
-    testFileName: '../../../webHwInterface/server-tests/hw7/server.js',
+    testFileName: '../../../jspilot/server-tests/hw7/server.js',
     module: 'web',
     emailTemplates: {}
 }  
