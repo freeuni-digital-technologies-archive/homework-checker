4154491be7f3e2c354cfacffa30a76e486320074
Luka Nadiradze <lnadi17@freeuni.edu.ge>
2022-11-15 20:52:14 +0400

    42-BUGFIX Fix get due date function

--- classroom-api/test/classroom-api.test.ts
+++ classroom-api/test/classroom-api.test.ts
@@ -1,14 +1,13 @@
 import { expect } from 'chai'
-import { ClassroomApi} from "../src";
-import {createDrive, downloadFile} from "../src/classroom-api";
+import {Authenticator, setupGoogleApi} from "../src";
+import {GoogleApi} from "dt-types";
 
 describe('test case', () => {
-    it('',  () => {
-        // ClassroomApi
-        //     .findClass('შესავალი ციფრულ ტექნოლოგიებში')
-        //     .then(classroom => classroom.listCourseWork())
-        //     .then(courseWork => console.log(courseWork))
-    })
-    
+    it('', async () => {
+        // const auth = new Authenticator("../../data/credentials/token.json", "../../data/credentials/credentials.json")
+        // const googleApi: GoogleApi = await setupGoogleApi(auth, '22f შესავალი ციფრულ ტექნოლოგიებში', "../../data/students.json")
+        // const dueDate = await googleApi.classroom.getDueDate('დავალება 1')
+        // console.log(dueDate);
+    }).timeout(30000)
 })
 
